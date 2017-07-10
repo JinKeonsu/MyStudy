e2ee294260141d1a016bf94b83c8dda56f5720a4
JinKeonsu <keonsu.jin@gmail.com>
2017-07-10 14:55:42 +0900

    martin source added

--- Tensorflow/test.py
+++
@@ -1,6 +0,0 @@
-
-import tensorflow as tf
-a = tf.constant(11)
-b = tf.constant(5)
-sess = tf.Session()
-print(sess.run(a+b))
--- Tensorflow/test3.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-hello = tf.constant("Hello, tensorFlow")
-sess = tf.Session()
-
-print(sess.run(hello))
