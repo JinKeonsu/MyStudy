f77e6f705718a5cf19c564b79f2f7f8b1599e917
Keonsu, Jin <keonsu.jin@gmail.com>
2020-06-16 16:09:35 +0900

    modified and created

---
+++ Python/2020_Study/0616.py
@@ -0,0 +1,20 @@
+#myScore, yourScore = 0.987, 0.453
+#print(myScore,yourScore)
+#print("your score is %f and my score is %f" % (yourScore, myScore))
+
+# myScore = [93,82,79]
+# othScore = 88
+# myScore.insert(2, othScore)
+# print(myScore)
+
+# mylist = [1205, 9988, "JinKwan", "Seoul"]
+# print("list: %s" % mylist)
+
+# astring = "Hello world"
+# rstring = astring[::-1]
+# sstring = astring.split(" ")
+# sstring.insert(1, rstring)
+# print(sstring)
+
+for i in range(1, 5,2):
+    print(i)
